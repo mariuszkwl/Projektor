da213cb3da278d9a0c9fedd8384e7f29da4d8a80
GeloUno <geloke59@gmail.com>
2018-10-12 14:45:46 +0200

    Hero Section *********************************** Add Home Section Hero Section *********************************** hero.component.html add image, text, button Sign In *********************************** hero.component.scss add posiotion to element add google fonts ********************************** home.component.html have hero section ********************************** nav.component.html add routerLink **********************************

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'test',
+    component: HomeComponent
+  },
+  {
+    path: 'lecture',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: HomeComponent
+  },
+  {
+    path: 'signup',
+    component: HomeComponent
+  },
+  {
+    path: 'signin',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { NavComponent } from './nav/nav.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HeroComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
