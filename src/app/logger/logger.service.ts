54bcbd7863de02a3310345d618cfda756f527085
GeloUno <geloke59@gmail.com>
2018-10-16 01:50:44 +0200

    Post login vol.2

--- src/app/logger/logger.service.ts
+++ src/app/logger/logger.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person } from '../models/pesron';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -22,16 +22,29 @@ export class LoggerService {
    * @returns ??
    */
   postLoginUser(email: String, password: String): Observable<Array<Body>> {
+    // const httpHeaders = new HttpHeaders().append(
+    //   'Content-Type',
+    //   'application/x-www-form-urlencoded; charset=UTF-8'
+    // );
     const body = new HttpParams()
-    .set('email', email + '')
+      .append('email', email + '')
       .append('password', password + '');
-      // .toString();
-
-    // const body = JSON.stringify({
-    //   emal: email,
-    //   pasword: password
-    // });
+    // .toString();
+    const body2 = {
+      email: email,
+      password: password
+    };
+    const body3 = JSON.stringify({
+      email: email,
+      pasword: password
+    });
     // const param = new HttpParams().set('userId', 1 + '');
-    return this.httpClient.post<Array<Body>>(this.urlServer, body);
+    return this.httpClient.post<Array<Body>>(
+      this.urlServer,
+      body3
+      //   {
+      //   headers: httpHeaders
+      // }
+    );
   }
 }
