406aa1f555c7c21f6c70a4ac9266d2317ee29341
GeloUno <geloke59@gmail.com>
2018-10-16 01:25:06 +0200

    With HttpParams

--- src/app/logger/logger.service.ts
+++ src/app/logger/logger.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person } from '../models/pesron';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggerService {
-   urlServer = ' https://stream-support.herokuapp.com/login';
+  urlServer = ' https://stream-support.herokuapp.com/login';
   // urlServer = 'https://jsonplaceholder.typicode.com/posts/';
   loggedUser = false;
   constructor(private httpClient: HttpClient) {}
@@ -22,12 +22,16 @@ export class LoggerService {
    * @returns ??
    */
   postLoginUser(email: String, password: String): Observable<Array<Body>> {
-    const body = JSON.stringify({
-      emal: email,
-      pasword: password
-    });
-   // const param = new HttpParams().set('userId', 1 + '');
+    const body = new HttpParams()
+    .set('email', email + '')
+      .append('password', password + '');
+      // .toString();
+
+    // const body = JSON.stringify({
+    //   emal: email,
+    //   pasword: password
+    // });
+    // const param = new HttpParams().set('userId', 1 + '');
     return this.httpClient.post<Array<Body>>(this.urlServer, body);
   }
 }
-
